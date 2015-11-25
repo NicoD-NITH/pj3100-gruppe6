e84d74887755f7a5897d0cc647bddf3ac79ca196
odinffs <odinffs@gmail.com>
2015-11-25 14:26:43 +0100

    grupperom c#

---
+++ PJAPP/Grupperom.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace PJAPP
+{
+    [Activity(Label = "Grupperom")]
+    public class Grupperom : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.gruppeRom);
+            // Create your application here
+        }
+    }
+}
