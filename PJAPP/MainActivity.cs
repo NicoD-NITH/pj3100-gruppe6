4b1e8079c09c0978e0797e2713e1b1a4820b5dbe
odinffs <odinffs@gmail.com>
2015-11-25 12:35:03 +0100

    første commit, user interface start

---
+++ PJAPP/MainActivity.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace PJAPP
+{
+    [Activity(Label = "PJAPP", MainLauncher = true, Icon = "@drawable/icon")]
+
+    public class MainActivity : Activity
+    {
+        private Button loggInnButton;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            
+            base.OnCreate(bundle);
+
+            SetContentView(Resource.Layout.Main);
+
+            loggInnButton = FindViewById<Button>(Resource.Id.loggInnButton);
+
+            loggInnButton.Click += delegate
+            {
+                StartActivity(typeof(MainPage));
+            };
+           
+        }
+    }
+}
+
