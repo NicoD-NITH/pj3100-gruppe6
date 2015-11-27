b888d32ef7d4ee5adecc0c1492f0cab84ad6064e
odinffs <odinffs@gmail.com>
2015-11-27 11:38:03 +0100

    småendringer

---
+++ PJAPP/RomBeacon.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace PJAPP
+{
+    public class RomBeacon
+    {
+        public int minor { get; set; }
+        public int major { get; set; }
+        public double distance { get; set; }
+        public string name { get; set; }
+    }
+}
---
+++ PJAPP/gruppeRomListAdapter.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace PJAPP
+{
+    class gruppeRomListAdapter : BaseAdapter<PJAPP.RomBeacon>
+    {
+        private List<PJAPP.RomBeacon> mItems;
+        private Context mContext;
+
+        public gruppeRomListAdapter(Context context, List<PJAPP.RomBeacon> items)
+        {
+            mItems = items;
+            mContext = context;
+        }
+        public override int Count
+        {
+            get
+            {
+                return mItems.Count;
+            }
+        }
+        public override long GetItemId(int position)
+        {
+            return position;
+        }
+        public override PJAPP.RomBeacon this[int position]
+        {
+            get
+            {
+                return mItems[position];
+            }
+        }
+        public override View GetView(int position, View convertView, ViewGroup parent)
+        {
+            View row = convertView;
+
+            if (row == null)
+            {
+                row = LayoutInflater.From(mContext).Inflate(PJAPP.Resource.Layout.gruppeRomRow, null, false);
+            }
+
+            TextView txtMinor = row.FindViewById<TextView>(Resource.Id.txtMinor);
+            txtMinor.Text = mItems[position].minor.ToString();
+
+            TextView txtMajor = row.FindViewById<TextView>(Resource.Id.txtMajor);
+            txtMajor.Text = mItems[position].major.ToString();
+
+            TextView txtDistance= row.FindViewById<TextView>(Resource.Id.txtDistance);
+            txtDistance.Text = mItems[position].distance.ToString() + "m";
+
+            TextView txtName = row.FindViewById<TextView>(Resource.Id.txtName);
+            txtName.Text = mItems[position].name;
+
+            return row;
+        }
+    }
+}
